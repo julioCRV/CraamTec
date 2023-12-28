51a1ec3a0f888fb605b1f35ec8dc7b627f645d55
julioCRV <julioroquevalda6@gmail.com>
2023-12-28 12:37:07 -0400

    Implementando Bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,14 @@ import './App.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Header from './components/Header/Header'
 
+import Pru from './components/HeaderBostrap/prueba'
+
 function App() {
   return (
     <Router>
       <div>
         <Header/>
+        <Pru/>
       </div>
     </Router>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import 'semantic-ui-css/semantic.min.css';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
