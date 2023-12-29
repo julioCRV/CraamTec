195d1fba759cbe3387080c43b10f391873b1519e
LeonAmericano101 <alexanderleon101@gmail.com>
2023-12-28 21:03:07 -0400

    cambios/Boostrap

--- src/components/Header/Inicio/Inicio.jsx
+++ src/components/Header/Inicio/Inicio.jsx
@@ -1,8 +1,48 @@
 import React from 'react';
+import { Container, Row, Col } from "react-bootstrap";
+import './inicio.css'
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import Badge from 'react-bootstrap/Badge';
 
 function Inicio() {
   return (
-    <h1>add mas diseño al INICIO</h1>
+    <section className="banner" id="home">
+      
+      <Container>
+        <Row className="align-items-center">
+          <Col md={12}>
+            <div className="vision-box">
+              <h2>Visión</h2>
+              <p><i>CRAAMTEC, nuestra misión conjunta es construir un ecosistema educativo y tecnológico que inspire
+              la excelencia y la innovación. Nos esforzamos cultivar mentes brillantes en la Academia Luminal,
+              brindando una educación integral que no solo nutre el conocimiento, sino también la creatividad y la
+              responsabilidad ética.
+              </i>        
+              </p>
+            </div>
+          </Col>
+        </Row>
+
+        <Row className="align-items-center">
+        <Col md={12}>
+            <div className="mision-box">
+              <h2>Misión</h2>
+              <p>
+                <i>
+                Nuestra vision e CRAAMTEC, es crear un uro on e a ucaci n y a tecno ogase entre acen
+                para impulsar la innovación y el progreso. Imaginamos un mundo donde cada mente cultivada en
+                CRAAMTEC se convierta en un agente de cambio, aplicando conocimientos éticos y habilidades
+                tecnológicas de programación para resolver desafíos globales.
+                </i>
+              </p>
+            </div>
+          </Col>
+         </Row>
+      </Container>
+
+    </section>
+    
   );
 }
 
--- src/components/HeaderBostrap/HeaderBostrap.jsx
+++ src/components/HeaderBostrap/HeaderBostrap.jsx
@@ -1,42 +1,59 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button, Navbar, Nav } from 'react-bootstrap';
+import './Header.css';
+import { Button, Navbar, Nav, Container } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
 
-import Routes from '../../routes/Routes'
+import Routes from '../../routes/Routes';
 
 function App() {
   return (
-    <div>
-    <Navbar expand="lg" bg="body-tertiary">
-      <div className="container-fluid">
-        <Navbar.Toggle aria-controls="navbarNav" />
-        <Navbar.Collapse id="navbarNav">
-          <Nav className="navbar-nav">
-            <Nav.Item>
-              <NavLink className="nav-link" activeClassName="active" to="/inicio">Inicio</NavLink>
-            </Nav.Item>
-            <Nav.Item>
-              <NavLink className="nav-link" activeClassName="active" to="/eventos">Eventos</NavLink>
-            </Nav.Item>
-            <Nav.Item>
-              <NavLink className="nav-link" activeClassName="active" to="/competencias">Competencias</NavLink>
-            </Nav.Item>
-            <Nav.Item>
-              <NavLink className="nav-link" activeClassName="active" to="/integrantes">Integrantes</NavLink>
-            </Nav.Item>
-            <Nav.Item>
-              <NavLink className="nav-link" activeClassName="active" to="/colaboradores">Colaboradores</NavLink>
-            </Nav.Item>
-            <Nav.Item>
-              <NavLink className="nav-link" activeClassName="active" to="/servicios">Servicios</NavLink>
-            </Nav.Item>
-          </Nav>
-        </Navbar.Collapse>
-      </div>
-    </Navbar>
-    <Routes />
-    </div>
+    <divc>
+      <Navbar expand="lg" bg="dark" variant="light" sticky="top" >
+        <Container>
+          <Navbar.Brand href="/">Tu Logo</Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarNav" />
+          <Navbar.Collapse id="navbarNav">
+            <Nav className="ml-auto">
+              <Nav.Item>
+                <NavLink className="nav-link" activeClassName="active" to="/inicio">
+                  Inicio
+                </NavLink>
+              </Nav.Item>
+              <Nav.Item>
+                <NavLink className="nav-link" activeClassName="active" to="/eventos">
+                  Eventos
+                </NavLink>
+              </Nav.Item>
+              <Nav.Item>
+                <NavLink className="nav-link" activeClassName="active" to="/competencias">
+                  Competencias
+                </NavLink>
+              </Nav.Item>
+              <Nav.Item>
+                <NavLink className="nav-link" activeClassName="active" to="/integrantes">
+                  Integrantes
+                </NavLink>
+              </Nav.Item>
+              <Nav.Item>
+                <NavLink className="nav-link" activeClassName="active" to="/colaboradores">
+                  Colaboradores
+                </NavLink>
+              </Nav.Item>
+              <Nav.Item>
+                <NavLink className="nav-link" activeClassName="active" to="/servicios">
+                  Servicios
+                </NavLink>
+              </Nav.Item>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <Container className="mt-3">
+        <Routes />
+      </Container>
+      
+    </divc>
   );
 }
 
