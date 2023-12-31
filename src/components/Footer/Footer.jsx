655e8aaa3bb7112e07772f202714684b1bbe075c
LeonAmericano101 <alexanderleon101@gmail.com>
2023-12-30 20:22:55 -0400

    inicio/foorte

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './Footer.css'; // Importa el archivo de estilos CSS
 
 const App = () => {
+  
   return (
     <div className="app-container">
       <footer className="footer">
