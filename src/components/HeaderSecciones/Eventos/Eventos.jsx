630f269db788c505ffeb6a981c7285b806b737c7
julioCRV <julioroquevalda6@gmail.com>
2024-01-01 15:18:52 -0400

    Implementando eventos

--- src/components/HeaderSecciones/Eventos/Eventos.jsx
+++ src/components/HeaderSecciones/Eventos/Eventos.jsx
@@ -1,8 +1,93 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Card from './Card';
+import axios from 'axios';
+
+const cardData = [
+  {
+    title: 'Card 1',
+    description: 'Description for Card 1',
+    imageUrl: 'https://placekitten.com/300/200', // Ejemplo de imagen
+  },
+  {
+    title: 'Card 2',
+    description: 'Description for Card 2',
+    imageUrl: 'https://placekitten.com/300/201', // Ejemplo de imagen
+  },
+  {
+    title: 'Card 2',
+    description: 'Description for Card 2',
+    imageUrl: 'https://placekitten.com/300/201', // Ejemplo de imagen
+  },
+  {
+    title: 'Card 2',
+    description: 'Description for Card 2',
+    imageUrl: 'https://placekitten.com/300/201', // Ejemplo de imagen
+  },
+  {
+    title: 'Card 2',
+    description: 'Description for Card 2',
+    imageUrl: 'https://placekitten.com/300/201', // Ejemplo de imagen
+  },
+
+  // Agrega más objetos al array según sea necesario
+];
 
 function Eventos() {
+  const [datos, setDatos] = useState([]);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('https://craamtec-web.onrender.com/events/api/events/totalEventos/');
+      setDatos(response.data);
+    } catch (error) {
+      console.error('Error al obtener los datos: ', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  console.log(datos)
+
   return (
-    <h1>add mas diseño al Eventos</h1>
+    <div className="container mt-5">
+      <div className="row">
+        {datos.map((dato) => (
+          <div className="col-md-4" key={dato.id}>
+            <Card titulo={dato.nombre_eve}
+              fecha={dato.fecha_eve}
+              enlaceI={dato.enlace_inscripcion_eve}
+              enlaceC={dato.enlace_chat_eve}
+              imagen={`https://drive.google.com/uc?id=${dato.foto_eve}`}
+              descripcion={dato.descripcion_eve}
+              participantes={dato.total_participantes_eve}
+              modalidad={dato.modalidad_eve}
+              tipo={dato.tipo_evento_eve}
+
+              plataforma={<div>
+                {dato.plataformas.map((plataforma) => (
+                  <div key={plataforma.id}>
+                    {plataforma.nombre_pla}
+                  </div>
+                ))}
+              </div>}
+
+              ubicacion={<div>
+                {dato.ubicaciones.map((ubi) => (
+                  <div key={ubi.id}>
+                    {ubi.nombre_ubi}
+
+                    {ubi.direccion_ubi}
+                  </div>
+                ))}
+              </div>}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+
   );
 }
 
--- src/components/HeaderSecciones/Servicios/Servicios.jsx
+++ src/components/HeaderSecciones/Servicios/Servicios.jsx
@@ -3,63 +3,60 @@ import React, { useState, useEffect } from 'react';
 import { v4 as uuidv4 } from "uuid";
 import Card from "./Card";
 import Carousel from "./Carousel";
+import axios from 'axios';
 
 function Servicios() {
   const [datos, setDatos] = useState([]);
-  const [loading, setLoading] = useState(true);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('https://craamtec-web.onrender.com/services/api/services/serviciosTotales/');
+      setDatos(response.data);
+    } catch (error) {
+      console.error('Error al obtener los datos: ', error);
+    }
+  };
 
   useEffect(() => {
-    async function fetchServicios() {
-      try {
-        const response = await fetch('https://craamtec-web.onrender.com/services/api/services/serviciosTotales/');
-        if (!response.ok) {
-          throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-        const result = await response.json();
-        setDatos(result);
-        setLoading(false); // Marcar la carga como completa
-      } catch (error) {
-        console.error('Error al obtener datos:', error);
-        setLoading(false); // Marcar la carga como completa incluso en caso de error
-      }
-    };
-
-    fetchServicios();
+    fetchData();
   }, []);
-
-  if (loading) {
-    return <p>Cargando datos...</p>;
-  }
 
   return (
     <div className="Servicios">
-      <Carousel
-        cards={datos.map((dato) => ({
-          key: uuidv4(),
-          content: <Card imagen={`https://drive.google.com/uc?id=${dato.imagen_ser}`} 
-          titulo={dato.nombre_ser} 
-          descripcion={dato.descripcion_ser} 
+      {datos.length > 0 ? (
+        <Carousel
+          cards={datos.map((dato) => ({
+            key: uuidv4(),
+            content: <Card imagen={`https://drive.google.com/uc?id=${dato.imagen_ser}`}
+              titulo={dato.nombre_ser}
+              descripcion={dato.descripcion_ser}
 
-          trabajos={<div>
-            {dato.trabajos.map((trabajo) => (
-              <div key={trabajo.id}>
-                <p>- {trabajo.nombre_tra}</p>
-              </div>
-            ))}
-          </div>}
+              trabajos={<div>
+                {dato.trabajos.map((trabajo) => (
+                  <div key={trabajo.id}>
+                    <p>- {trabajo.nombre_tra}</p>
+                  </div>
+                ))}
+              </div>}
 
-          celular={<div>
-            <div key={dato.celular.id}>
-              <p>{dato.celular.numero_cel}</p>
-            </div>
-          </div>}
-          />,
-        }))}
-        width="75%"
-        margin="0px auto"
-        offset={2}
-        showArrows={false}
-      />
+              celular={<div>
+                <div key={dato.celular.id}>
+                  <p>{dato.celular.numero_cel}</p>
+                </div>
+              </div>}
+            />,
+          }))}
+          width="75%"
+          margin="0px auto"
+          offset={2}
+          showArrows={false}
+        />
+      ) : (
+        <p>Cargando datos...</p>
+      )}
+
+
+
     </div>
   );
 }
