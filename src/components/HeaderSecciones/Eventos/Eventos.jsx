981beeca54a1d56224b9623fcc64cdbb016e8bb0
julioCRV <julioroquevalda6@gmail.com>
2024-01-04 23:30:33 -0400

    detalles

--- src/components/HeaderSecciones/Eventos/Eventos.jsx
+++ src/components/HeaderSecciones/Eventos/Eventos.jsx
@@ -21,23 +21,24 @@ function Eventos() {
 
   return (
     <div className="container mt-5">
-      <div className="row">
-        {datos.map((dato) => (
-          <div className="col-md-4" key={dato.id}>
-            <Card titulo={dato.nombre_eve}
-              fecha={dato.fecha_eve}
-              // enlaceI={dato.enlace_inscripcion_eve}
-              enlaceI={`${dato.enlace_inscripcion_eve}`}
-              enlaceC={`${dato.enlace_chat_eve}`}
-              imagen={`https://drive.google.com/uc?id=${dato.foto_eve}`}
-              descripcion={dato.descripcion_eve}
-              participantes={dato.total_participantes_eve}
-              modalidad={dato.modalidad_eve}
-              tipo={dato.tipo_evento_eve}
+      {datos.length > 0 ? (
+        <div className="row">
+          {datos.map((dato) => (
+            <div className="col-md-4" key={dato.id}>
+              <Card titulo={dato.nombre_eve}
+                fecha={dato.fecha_eve}
+                // enlaceI={dato.enlace_inscripcion_eve}
+                enlaceI={`${dato.enlace_inscripcion_eve}`}
+                enlaceC={`${dato.enlace_chat_eve}`}
+                imagen={`https://drive.google.com/uc?id=${dato.foto_eve}`}
+                descripcion={dato.descripcion_eve}
+                participantes={dato.total_participantes_eve}
+                modalidad={dato.modalidad_eve}
+                tipo={dato.tipo_evento_eve}
 
-              plataforma={dato.plataformas.map((plataforma) => plataforma.nombre_pla)}
+                plataforma={dato.plataformas.map((plataforma) => plataforma.nombre_pla)}
 
-              ubicacion={dato.ubicaciones.map((ubi) => ubi.nombre_ubi)}
+                ubicacion={dato.ubicaciones.map((ubi) => ubi.nombre_ubi)}
               // ubicacion={<div>
               //   {dato.ubicaciones.map((ubi) => (
               //     <div key={ubi.id}>
@@ -46,10 +47,13 @@ function Eventos() {
               //     </div>
               //   ))}
               // </div>}
-            />
-          </div>
-        ))}
-      </div>
+              />
+            </div>
+          ))}
+        </div>
+      ) : (
+        <p>Cargando datos...</p>
+      )}
     </div>
 
   );
